84fc18b3cdbf52ed8478929d138bab9c77b855f0
Sankichi <info@sankichi.net>
2015-07-17 11:44:03 +0900

    デバッグ

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   def self.find_or_create_from_auth_hash(auth_hash)
     provider = auth_hash[:provider]
     uid = auth_hash[:uid]
-    nickname = auth_hash[:nickname]
-    image_url = auth_hash[:image]
+    nickname = auth_hash[:info][:nickname]
+    image_url = auth_hash[:info][:image]
 
     User.find_or_create_by(provider: provider, uid: uid) do |user|
       user.nickname = nickname
